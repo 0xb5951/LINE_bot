0427b143fe19eaf5bc5f1b597b4dc76bb7ccef3f
MizushimaKeita <mizushima.keita@lancers.co.jp>
2019-08-16 18:58:20 +0900

    フォローされた場合とテキスト受信で処理を分離

--- auto_easy_reply_bot/main.js
+++ auto_easy_reply_bot/main.js
@@ -6,27 +6,35 @@ function doPost(e) {
     var ss = SpreadsheetApp.openById(SpreadsheetApp.getActiveSpreadsheet().getId());
     var sheet = ss.getSheetByName('sheet1');
 
-    // WebHookで受信した応答用Token
-    var replyToken = JSON.parse(e.postData.contents).events[0].replyToken;
-    // ユーザーのメッセージを取得
-    var userMessage = JSON.parse(e.postData.contents).events[0].message.text;
 
-    // 応答メッセージ用のAPI URL
-    var url = 'https://api.line.me/v2/bot/message/reply';
+    // ユーザーにbotがフォローされた場合の処理
+    if (event.type == 'follow') {
+    }
 
-    UrlFetchApp.fetch(url, {
-        'headers': {
-            'Content-Type': 'application/json; charset=UTF-8',
-            'Authorization': 'Bearer ' + ACCESS_TOKEN,
-        },
-        'method': 'post',
-        'payload': JSON.stringify({
-            'replyToken': replyToken,
-            'messages': [{
-                'type': 'text',
-                'text': userMessage + 'ンゴ',
-            }],
-        }),
-    });
-    return ContentService.createTextOutput(JSON.stringify({ 'content': 'post ok' })).setMimeType(ContentService.MimeType.JSON);
+    // テキストが送信された時の処理
+    if (event.type == 'message') {
+        // WebHookで受信した応答用Token
+        var replyToken = JSON.parse(e.postData.contents).events[0].replyToken;
+        // ユーザーのメッセージを取得
+        var userMessage = JSON.parse(e.postData.contents).events[0].message.text;
+
+        // 応答メッセージ用のAPI URL
+        var url = 'https://api.line.me/v2/bot/message/reply';
+
+        UrlFetchApp.fetch(url, {
+            'headers': {
+                'Content-Type': 'application/json; charset=UTF-8',
+                'Authorization': 'Bearer ' + ACCESS_TOKEN,
+            },
+            'method': 'post',
+            'payload': JSON.stringify({
+                'replyToken': replyToken,
+                'messages': [{
+                    'type': 'text',
+                    'text': userMessage + 'ンゴ',
+                }],
+            }),
+        });
+        return ContentService.createTextOutput(JSON.stringify({ 'content': 'post ok' })).setMimeType(ContentService.MimeType.JSON);
+    }
 }
