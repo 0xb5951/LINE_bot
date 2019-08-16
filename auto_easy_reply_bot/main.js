1b2f350a0cde9c239c689b8cf832a3c2ecde4b36
MizushimaKeita <mizushima.keita@lancers.co.jp>
2019-08-16 19:35:12 +0900

    処理を関数化

--- auto_easy_reply_bot/main.js
+++ auto_easy_reply_bot/main.js
@@ -2,39 +2,59 @@
 var ACCESS_TOKEN = 'token';
 
 function doPost(e) {
+    var event = JSON.parse(e.postData.contents).events[0];
+    var replyToken = event.replyToken;
+
     // シート取得
     var ss = SpreadsheetApp.openById(SpreadsheetApp.getActiveSpreadsheet().getId());
-    var sheet = ss.getSheetByName('sheet1');
+    var sheet = ss.getSheetByName('登録ユーザ一覧');
 
+    // WebHookで受信した応答用Token
+    var replyToken = JSON.parse(e.postData.contents).events[0].replyToken;
 
     // ユーザーにbotがフォローされた場合の処理
     if (event.type == 'follow') {
+        followMessage(replyToken);
     }
 
     // テキストが送信された時の処理
     if (event.type == 'message') {
-        // WebHookで受信した応答用Token
-        var replyToken = JSON.parse(e.postData.contents).events[0].replyToken;
         // ユーザーのメッセージを取得
         var userMessage = JSON.parse(e.postData.contents).events[0].message.text;
 
-        // 応答メッセージ用のAPI URL
-        var url = 'https://api.line.me/v2/bot/message/reply';
 
-        UrlFetchApp.fetch(url, {
-            'headers': {
-                'Content-Type': 'application/json; charset=UTF-8',
-                'Authorization': 'Bearer ' + ACCESS_TOKEN,
-            },
-            'method': 'post',
-            'payload': JSON.stringify({
-                'replyToken': replyToken,
-                'messages': [{
-                    'type': 'text',
-                    'text': userMessage + 'ンゴ',
-                }],
-            }),
-        });
-        return ContentService.createTextOutput(JSON.stringify({ 'content': 'post ok' })).setMimeType(ContentService.MimeType.JSON);
+        sendMessage(replyToken, userMessage);
     }
+    return ContentService.createTextOutput(JSON.stringify({ 'content': 'post ok' })).setMimeType(ContentService.MimeType.JSON);
+}
+
+
+// 登録時のアンケート導線
+function followMessage(replyToken) {
+    var sendtext = "NOIABの先行体験をご希望の方は下記をご記入ください！\n \
+    楽器やレベルで対象に選出されましたら別途先行体験のご案内を送らさせていただきます。";
+
+    sendMessage(replyToken, sendtext);
+    return 0;
+}
+
+function sendMessage(replyToken, message) {
+    // 応答メッセージ用のAPI URL
+    var url = 'https://api.line.me/v2/bot/message/reply';
+
+    UrlFetchApp.fetch(url, {
+        'headers': {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Authorization': 'Bearer ' + ACCESS_TOKEN,
+        },
+        'method': 'post',
+        'payload': JSON.stringify({
+            'replyToken': replyToken,
+            'messages': [{
+                'type': 'text',
+                'text': message + 'ンゴ',
+            }],
+        }),
+    });
+    return 0;
 }
